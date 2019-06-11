422662f37cec21188da1b1326186169f4fb51d8b
Matt Burger <matt.burger1@outlook.com>
2019-06-11 01:45:12 -0700

    PseudoQueue code complete. Tests under dev.

--- code401Challenges/src/main/java/code401Challenges/stackandqueues/pseudoQueue/PseudoQueue.java
+++ code401Challenges/src/main/java/code401Challenges/stackandqueues/pseudoQueue/PseudoQueue.java
@@ -1,8 +1,9 @@
 package code401Challenges.stackandqueues.pseudoQueue;
 
+import code401Challenges.stackandqueues.Node;
 import code401Challenges.stackandqueues.Stack;
 
-public class PseudoQueue {
+public class PseudoQueue<T> {
     protected Stack s1;
     protected Stack s2;
 
@@ -11,10 +12,36 @@ public class PseudoQueue {
         s2 = new Stack();
     }
 
-    public boolean isEmpty(Stack s){
+    public boolean isStackEmpty(Stack s){
         if(s.getTop() == null){
             return true;
         }
         return false;
     }
+
+    public Stack getStack1(){
+        return this.s1;
+    }
+
+    public Stack getStack2(){
+        return this.s2;
+    }
+
+    public void enqueue(T value){
+
+        if(isStackEmpty(s2)){
+            s2.push(value);
+        } else {
+            while(s2.getTop() != null){
+                s1.push( s2.pop() );
+            }
+            s1.push(value);
+
+            while(s1.getTop() != null){
+                s2.push( s1.pop() );
+            }
+        }
+    }
+
+
 }
--- code401Challenges/src/test/java/code401Challenges/stackandqueues/pseudoQueue/TestPseudoQueue.java
+++ code401Challenges/src/test/java/code401Challenges/stackandqueues/pseudoQueue/TestPseudoQueue.java
@@ -1,4 +1,21 @@
 package code401Challenges.stackandqueues.pseudoQueue;
 
+import code401Challenges.stackandqueues.Stack;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class TestPseudoQueue {
+    @Before
+    public void TestPseudoQueueSetup(){
+        PseudoQueue queue = new PseudoQueue();
+        System.out.println("Setup complete.");
+    }
+
+    @Test
+    public void TestConstructor(){
+        PseudoQueue queue = new PseudoQueue();
+        assertTrue(queue.isStackEmpty(queue.getStack1()));
+        assertTrue(queue.isStackEmpty(queue.getStack2()));
+    }
 }
