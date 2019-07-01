3c189db6f810d86a3d33cf5f51dfdc6502b3a103
Matt Burger <matt.burger1@outlook.com>
2019-07-01 10:15:50 -0700

    tests pass

--- code401Challenges/src/main/java/code401Challenges/insertionSort/InsertionSort.java
+++ code401Challenges/src/main/java/code401Challenges/insertionSort/InsertionSort.java
@@ -2,31 +2,36 @@ package code401Challenges.insertionSort;
 
 public class InsertionSort {
 
-    public int[] insertionSortMethod(int[] inputArr) {
-        if(inputArr.length < 1) {
+    int[] arrToSort;
+
+    public InsertionSort(int[] inputArr) {
+        this.arrToSort = inputArr;
+    }
+    public int[] insertionSortMethod() {
+        if(this.arrToSort.length < 1) {
             throw new IllegalStateException("Cannot sort an empty array!");
         }
 
-        else if(inputArr.length == 1) {
-            return inputArr;
+        else if(this.arrToSort.length == 1) {
+            return this.arrToSort;
         }
 
         else {
 
-            for (int i = 1; i < inputArr.length; i++) {
+            for (int i = 1; i < this.arrToSort.length; i++) {
 
                 int j = i - 1;
-                int temp = inputArr[i];
+                int temp = this.arrToSort[i];
 
-                while(j >= 0 && temp < inputArr[j]) {
-                    inputArr[j + 1] = inputArr[j];
+                while(j >= 0 && temp < this.arrToSort[j]) {
+                    this.arrToSort[j + 1] = this.arrToSort[j];
                     j = j - 1;
                 }
 
-                inputArr[j + 1] = temp;
+                this.arrToSort[j + 1] = temp;
             }
 
-            return inputArr;
+            return this.arrToSort;
 
         }
     }
--- code401Challenges/src/test/java/code401Challenges/insertionSort/TestInsertionSort.java
+++ code401Challenges/src/test/java/code401Challenges/insertionSort/TestInsertionSort.java
@@ -1,7 +1,69 @@
 package code401Challenges.insertionSort;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class TestInsertionSort {
 
-    
+    @Test
+    public void TestInsertionSort() {
+        int[] inputArr = new int[] {5, 4, 6, 3, 1, 2};
+        InsertionSort insertionSort = new InsertionSort(inputArr);
+        int[] outputArr = insertionSort.insertionSortMethod();
+        int[] expectedArr = new int[]{1, 2, 3, 4, 5, 6};
 
+        assertArrayEquals("The resulting array should be returned sorted.", expectedArr, outputArr);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void TestInsertionSort_emptyArr() {
+        int[] inputArr = new int[] {};
+        InsertionSort insertionSort = new InsertionSort(inputArr);
+        int[] outputArr = insertionSort.insertionSortMethod();
+        int[] expectedArr = new int[]{1, 2, 3, 4, 5, 6};
+
+        assertArrayEquals("The resulting array should be returned sorted.", expectedArr, outputArr);
+    }
+
+    @Test
+    public void TestInsertionSort_arrOfSizeOne() {
+        int[] inputArr = new int[] {5};
+        InsertionSort insertionSort = new InsertionSort(inputArr);
+        int[] outputArr = insertionSort.insertionSortMethod();
+
+        assertArrayEquals("The resulting array should be returned sorted.", inputArr, outputArr);
+    }
+
+    @Test
+    public void TestInsertionSort_arrOfSomeIdenticalValues() {
+        int[] inputArr = new int[] {5, 4, 3, 3, 2, 6, 7, 1, 1};
+        InsertionSort insertionSort = new InsertionSort(inputArr);
+        int[] outputArr = insertionSort.insertionSortMethod();
+        int[] expectedArr = new int[]{1, 1, 2, 3, 3, 4, 5, 6, 7};
+
+        assertArrayEquals("The resulting array should be returned sorted.", inputArr, outputArr);
+    }
+
+    @Test
+    public void TestInsertionSort_arrOfTwoIdenticalValues() {
+        int[] inputArr = new int[] {5, 5};
+        InsertionSort insertionSort = new InsertionSort(inputArr);
+        int[] outputArr = insertionSort.insertionSortMethod();
+        int[] expectedArr = new int[]{5, 5};
+
+        assertArrayEquals("The resulting array should be returned sorted.", inputArr, outputArr);
+    }
+
+    @Test
+    public void TestInsertionSort_arrOfTwoValues() {
+        int[] inputArr = new int[] {5, 3};
+        InsertionSort insertionSort = new InsertionSort(inputArr);
+        int[] outputArr = insertionSort.insertionSortMethod();
+        int[] expectedArr = new int[]{3, 5};
+
+        assertArrayEquals("The resulting array should be returned sorted.", inputArr, outputArr);
+    }
 }
+
+
