29dd1d457d1d0341be9220deed814db9d3dbf79a
Matt Burger <matt.burger1@outlook.com>
2019-07-19 10:30:30 -0700

    Graph implemented. Tests in progress

--- code401Challenges/src/main/java/code401Challenges/graph/Edge.java
+++ code401Challenges/src/main/java/code401Challenges/graph/Edge.java
@@ -1,39 +1,23 @@
 package code401Challenges.graph;
 
 public class Edge {
-    private Graph source;
     private Graph destination;
     private int weight;
 
-    public Edge(Graph source, Graph destination, int weight) {
-        this.source = source;
+    public Edge(Graph destination, int weight) {
         this.destination = destination;
         this.weight = weight;
     }
 
     //Getters
-    public Graph getSource() {
-        return this.source;
-    }
-
-    public Graph getDestination() {
-        return this.destination;
-    }
+    public Graph getDestination() { return this.destination; }
 
     public int getWeight() {
         return this.weight;
     }
 
     //Setters
-    public void setSource(Graph source) {
-        this.source = source;
-    }
+    public void setDestination(Graph destination) { this.destination = destination; }
 
-    public void setDestination(Graph destination) {
-        this.destination = destination;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
+    public void setWeight(int weight) { this.weight = weight; }
 }
--- code401Challenges/src/main/java/code401Challenges/graph/Graph.java
+++ code401Challenges/src/main/java/code401Challenges/graph/Graph.java
@@ -1,13 +1,10 @@
 package code401Challenges.graph;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Graph<T> {
     private T data;
-    private List<Edge> edges;
-    private List neighbors;
+    private LinkedList<Edge> neighbors;
     private List allNodes;
 
     public Graph(T data) {
@@ -16,21 +13,63 @@ public class Graph<T> {
         this.allNodes = new ArrayList();
     }
 
-    public Graph addNode(Graph node) {
+    public Graph addNode(Graph node, int weight) {
         if(!this.neighbors.contains(node)) {
-            this.neighbors.add(node);
-            node.neighbors.add(this);
-
-            this.addEdge(this, node, 100);
+            this.neighbors.add(new Edge(node, weight) );
+            node.neighbors.add(new Edge(this, weight) );
         }
 
         return node;
     }
 
-    public void addEdge(Graph src, Graph dest, int weight) {
-        src.edges.add(new Edge(src, dest, weight) );
+    public Edge addEdge(Graph dest, int weight) {
+        return new Edge(dest, weight);
     }
 
+    public List<Graph> getNeighbors(Graph node) {
+        return node.neighbors;
+    }
+
+    public List<Graph> getNodes(Graph node) {
+        List<Graph> allNodes;
+        Stack<Graph> visted = new Stack<>();
+        Stack<Graph> toVist = new Stack<>();
+
+        visted.push(node);
+        toVist.push(node);
+
+        while( !toVist.isEmpty() ) {
+            if( !toVist.peek().neighbors.isEmpty() ) {
+                 LinkedList<Edge> currentNodeNeighbors = toVist.peek().neighbors;
+                 Graph tmpNode = currentNodeNeighbors.poll().getDestination();
+
+                 while( visted.contains(tmpNode) && !currentNodeNeighbors.isEmpty() ) {
+                     tmpNode = currentNodeNeighbors.poll().getDestination();
+                 }
+
+                 if(currentNodeNeighbors.isEmpty()) {
+                     toVist.pop();
+                 }
+
+                 else if( !visted.contains(tmpNode) ) {
+                     visted.add(tmpNode);
+                     toVist.push(tmpNode);
+                 } else {
+                     tmpNode = currentNodeNeighbors.poll().getDestination();
+                 }
+            } else {
+                toVist.pop();
+            }
+        }
+
+        allNodes = new ArrayList<>(visted);
+
+        return allNodes;
+    }
+
+    public int size() {
+        return getNodes(this).size();
+    }
 
     //Getters
     public T getData() {
@@ -46,7 +85,7 @@ public class Graph<T> {
         this.data = data;
     }
 
-    public void setNeighbors(List neighbors) {
+    public void setNeighbors(LinkedList neighbors) {
         this.neighbors = neighbors;
     }
 }
